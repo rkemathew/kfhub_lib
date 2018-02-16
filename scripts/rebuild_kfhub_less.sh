7ee09b08c2a8527b163550d96964cdf07a582753
Ronnie Mathew <ronnie@bricksimple.com>
2018-02-16 11:19:04 -0500

    Made modifications to the package.json to perform the sync of the assets languages and less folders using the npm sync module instead of the custom shell scripts

--- scripts/rebuild_kfhub_less.sh
+++
@@ -1,2 +0,0 @@
-cp -ruv ../kfhub_app/src/styles.less ./src/
-cp -ruv ../kfhub_app/src/app/modules/shared/_kfcore-less/* ./src/app/modules/shared/_kfcore-less/
--- scripts/rebuild_kfhub_lib.sh
+++
@@ -1,8 +0,0 @@
-DIR=`pwd`
-cd ../kfhub_lib
-npm run packagr
-cd dist
-npm pack
-cd $DIR
-rm -rf ./node_modules/kfhub_lib
-npm install
